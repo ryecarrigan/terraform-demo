7f689fabbffe12fb3bc866b915c7167b31e38cd0
Ryan Carrigan <r.carrigan@arca.com>
2017-07-20 10:02:09 -0400

    VPC Demo
    Renaming folders

---
+++ README.md
@@ -0,0 +1,2 @@
+# Terraform Demo
+This is a demonstration of running a web application in a secure infrastructure
--- backend/index.php
+++ backend/index.php
@@ -4,6 +4,5 @@ $database   = $_ENV['POSTGRES_DB'];
 $username   = $_ENV['POSTGRES_USER'];
 $password   = $_ENV['POSTGRES_PASSWORD'];
 $connection = new PDO("pgsql:host=${hostname};dbname=${database}", $username, $password);
-$status     = $connection->getAttribute(PDO::ATTR_CONNECTION_STATUS);
 
-echo "PostgreSQL connection: ${status}";
+echo $connection->getAttribute(PDO::ATTR_CONNECTION_STATUS);
