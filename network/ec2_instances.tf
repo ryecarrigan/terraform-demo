679c3b0cbd1221ba311867c156098c4f30c38049
Ryan Carrigan <r.carrigan@arca.com>
2017-07-28 15:43:08 -0400

    Adding Network ACLs
    Fix for SGs on NAT instance

--- network/ec2_instances.tf
+++ network/ec2_instances.tf
@@ -1,11 +1,11 @@
 // Create a NAT gateway.
 resource "aws_instance" "nat" {
-  ami               = "${data.aws_ami.nat.id}"
-  instance_type     = "t2.nano"
-  key_name          = "${var.key_name}"
-  security_groups   = ["${aws_security_group.nat.id}", "${aws_security_group.ssh.id}", "${aws_security_group.vpc.id}"]
-  source_dest_check = false
-  subnet_id         = "${aws_subnet.public_a.id}"
+  ami                    = "${data.aws_ami.nat.id}"
+  instance_type          = "t2.nano"
+  key_name               = "${var.key_name}"
+  source_dest_check      = false
+  subnet_id              = "${aws_subnet.public_a.id}"
+  vpc_security_group_ids = ["${aws_security_group.nat.id}", "${aws_security_group.ssh.id}", "${aws_security_group.vpc.id}"]
 
   tags {
     Name = "${var.prefix}: NAT Gateway"
